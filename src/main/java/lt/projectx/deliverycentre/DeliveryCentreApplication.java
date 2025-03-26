93331b7ad7a5063bc641b7978b461ee4b667c096
nadezda <nadezda@mail.com>
2025-03-26 19:18:46 +0200

    Initial commit

---
+++ src/main/java/lt/projectx/deliverycentre/DeliveryCentreApplication.java
@@ -0,0 +1,13 @@
+package lt.projectx.deliverycentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryCentreApplication.class, args);
+	}
+
+}
